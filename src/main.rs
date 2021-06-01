04170d3dffd2de4e6294cf099dafdbd3962f3d7c
NaokiM03 <naoki.matsumura@sansan.com>
2021-06-02 03:18:02 +0900

    impl: github

--- src/main.rs
+++ src/main.rs
@@ -143,11 +143,53 @@ mod test_get_twitter_url {
     }
 }
 
+fn get_github_url(s: &str) -> String {
+    let strs = s.split_whitespace().collect::<Vec<&str>>();
+
+    let mut url = "https://github.com".to_string();
+
+    if strs.len() == 1 {
+    } else if strs.contains(&"--me") {
+        url.push_str("/NaokiM03");
+    } else {
+        url.push_str(&format!("/{}", strs[1]));
+    }
+
+    url
+}
+
+#[cfg(test)]
+mod test_get_github_url {
+    use super::*;
+
+    #[test]
+    fn test_get_github_url() {
+        let actual = get_github_url("gh");
+        let expected = "https://github.com";
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn test_get_github_url_with_me() {
+        let actual = get_github_url("gh --me");
+        let expected = "https://github.com/NaokiM03";
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn test_get_github_url_with_keyword() {
+        let actual = get_github_url("gh NaokiM03/akashic-records");
+        let expected = "https://github.com/NaokiM03/akashic-records";
+        assert_eq!(actual, expected);
+    }
+}
+
 #[get("/search")]
 async fn search(query: web::Query<Query>) -> impl Responder {
     let cmd_first_str = get_command(&query.cmd);
     let url = match cmd_first_str {
         "tw" => get_twitter_url(&query.cmd),
+        "gh" => get_github_url(&query.cmd),
         _ => get_google_url(&query.cmd),
     };
     HttpResponse::TemporaryRedirect()
