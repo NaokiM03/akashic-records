d68ca8de48996259cf7e4b1eaa89cc971829ecb4
NaokiM03 <naoki.matsumura@sansan.com>
2021-06-01 19:40:52 +0900

    return query parameter

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct Query {
+    cmd: String,
+}
+
+#[get("/search")]
+async fn search(query: web::Query<Query>) -> impl Responder {
+    HttpResponse::Ok().body(&query.cmd)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(search))
+        .bind("127.0.0.1:3000")?
+        .run()
+        .await
 }
