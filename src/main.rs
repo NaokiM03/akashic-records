568059095538c83b664e626129171eb8a13b5861
NaokiM03 <naoki.matsumura@sansan.com>
2021-06-02 02:54:24 +0900

    impl: twitter search

--- src/main.rs
+++ src/main.rs
@@ -87,10 +87,67 @@ mod test_get_google_url {
     }
 }
 
+fn get_twitter_url(s: &str) -> String {
+    let strs = s.split_whitespace().collect::<Vec<&str>>();
+
+    if strs.len() == 1 {
+        "https://twitter.com/home".to_string()
+    } else {
+        let is_no_reply = strs.contains(&"--no-reply");
+        let is_live = strs.contains(&"--now");
+
+        // "%20OR%20%40i" == " OR @i"
+        let mut url = format!("https://twitter.com/search?q={}%20OR%20%40i", strs[1]);
+        if is_no_reply {
+            // "%20-filter%3Areplies" == " -filter:replies"
+            url.push_str("%20-filter%3Areplies");
+        }
+        if is_live {
+            url.push_str("&f=live");
+        }
+
+        url
+    }
+}
+
+#[cfg(test)]
+mod test_get_twitter_url {
+    use super::*;
+
+    #[test]
+    fn test_get_twitter_url() {
+        let actual = get_twitter_url("tw");
+        let expected = "https://twitter.com/home";
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn test_get_twitter_url_with_keyword() {
+        let actual = get_twitter_url("tw foo");
+        let expected = "https://twitter.com/search?q=foo%20OR%20%40i";
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn test_get_twitter_url_with_keyword_and_no_reply() {
+        let actual = get_twitter_url("tw foo --no-reply");
+        let expected = "https://twitter.com/search?q=foo%20OR%20%40i%20-filter%3Areplies";
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn test_get_twitter_url_with_keyword_and_now() {
+        let actual = get_twitter_url("tw foo --now");
+        let expected = "https://twitter.com/search?q=foo%20OR%20%40i&f=live";
+        assert_eq!(actual, expected);
+    }
+}
+
 #[get("/search")]
 async fn search(query: web::Query<Query>) -> impl Responder {
-    let cmd_first_str = get_command(&query.cmd).to_owned();
+    let cmd_first_str = get_command(&query.cmd);
     let url = match cmd_first_str {
+        "tw" => get_twitter_url(&query.cmd),
         _ => get_google_url(&query.cmd),
     };
     HttpResponse::TemporaryRedirect()
